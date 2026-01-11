ef5c22420c53282c933645fa0c3adfac3d58f9b0
omkar1010 <orkarande_b24@it.vjti.ac.in>
2026-01-12 00:22:02 +0530

    changes

---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Card = (props) => {
+  return (
+    <div className='mr-5 bg-white text-black inline-block p-6 text-center rounder-br'>
+       <h2>Hello</h2>
+    </div>
+  )
+}
+
+export default Card
